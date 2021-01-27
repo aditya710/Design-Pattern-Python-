42e7275a2410c4121f95ec930483bdf28dc40e2f
Aditya Shelar <adityashelar710@gmail.com>
2021-01-27 01:21:44 +0100

    Added Factory and Singleton

--- Ex_Files_python_dpatterns/Exercise Files/Ch02/02_02/factory.py
+++ Ex_Files_python_dpatterns/Exercise Files/Ch02/02_02/factory.py
@@ -6,4 +6,27 @@ class Dog:
 		self._name = name
 
 	def speak(self):
-		return "Woof!"
+		return ("My name is {0} and I do Woof:".format(self._name))
+
+class Cat:
+
+	"""A simple cat class"""
+
+	def __init__(self, name):
+		self._name = name
+
+	def speak(self):
+		return ("My name is {0} and I do Meow:".format(self._name))
+
+def get_pet(pet):
+	"""The Factory method"""
+
+	pets = dict(dog = Dog("Hope"), cat = Cat("Peace"))
+
+	return pets[pet]
+
+d = get_pet("dog")
+print(d.speak())
+
+d = get_pet("cat")
+print(d.speak())
--- Ex_Files_python_dpatterns/Exercise Files/Ch02/02_06/singleton.py
+++ Ex_Files_python_dpatterns/Exercise Files/Ch02/02_06/singleton.py
@@ -1,12 +1,26 @@
 class Borg:
-    
+    _shared_state = {}
+
+    def __init__(self):
+        self.__dict__ = self._shared_state
     """The Borg design pattern"""
     
 
-        
+class Singleton(Borg):
 
-class Singleton(Borg): 
-    
     """The singleton class"""
-   
+
+    def __init__(self, **kwargs):
+        Borg.__init__(self)
+        self._shared_state.update(kwargs)
+
+    def __str__(self):
+        return str(self._shared_state)
+
+
+x = Singleton(HTTP = "HyperText Transfer Protocol")
+print(x)
+
+y = Singleton(FTP = "File Transfer Protocol")
+print(y)
 
